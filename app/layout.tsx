e369a90efd6d33b1743afc50163f863bc762ce6d
RizinGH <rizi.n2021@gmail.com>
2024-06-07 21:40:14 +0530

    layout changes

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,6 +1,7 @@
 import type { Metadata } from "next";
 import { Manrope } from "next/font/google";
 import "./globals.css";
+import React from "react";
 
 const font = Manrope({ subsets: ["latin"] });
 
